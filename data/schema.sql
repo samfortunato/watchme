7a67dabd1ec26a6bf049afc417942f0df3b85d9b
Samuel Fortunato <samfortunato@gmail.com>
2025-11-04 23:19:29 -0800

    add comments

--- data/schema.sql
+++ data/schema.sql
@@ -1,7 +1,26 @@
 create table if not exists videos (
 	id serial primary key,
+
 	title text not null,
 	description text not null,
 	url text not null,
-	created_at timestamp default now()
+
+	created_at timestamp default now(),
+	updated_at timestamp default now()
+);
+
+create table if not exists comments (
+	id serial primary key,
+
+	body text not null,
+
+	created_at timestamp default now(),
+	updated_at timestamp default now()
+);
+
+create table if not exists video_comments (
+	id serial primary key,
+
+	comment_id integer not null references comments(id) on delete cascade,
+	video_id integer not null references videos(id) on delete cascade
 );
