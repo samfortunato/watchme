8e6172ba2c0fb8b223e79f4a1e598ff50a95a404
Samuel Fortunato <samfortunato@gmail.com>
2025-11-04 16:19:41 -0800

    add basic video display

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-    <h1>hello, world</h1>
+    <main>
+      <article className="video flex flex-col gap-1">
+        <h2 className="order-2 font-bold">Test</h2>
+
+        <h3 className="order-3 text-sm">Description</h3>
+        <p className='order-4 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis veniam eligendi est eius cupiditate illum nulla, atque sit eveniet. Ab delectus eveniet exercitationem corrupti assumenda, neque voluptates fugit consequatur officia!</p>
+
+        <video className="order-first" src="./test.mp4" controls></video>
+      </article>
+    </main>
   );
 }
