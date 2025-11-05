be92196aa129301665462a10ee5c0c0b46c80bb6
Samuel Fortunato <samfortunato@gmail.com>
2025-11-04 17:35:34 -0800

    remove styling from components for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <main>
-      <article className="video flex flex-col gap-1">
-        <h2 className="order-2 font-bold">Test</h2>
+      <article>
+        <h2>Test</h2>
 
-        <h3 className="order-3 text-sm">Description</h3>
-        <p className='order-4 text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis veniam eligendi est eius cupiditate illum nulla, atque sit eveniet. Ab delectus eveniet exercitationem corrupti assumenda, neque voluptates fugit consequatur officia!</p>
+        <h3>Description</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis veniam eligendi est eius cupiditate illum nulla, atque sit eveniet. Ab delectus eveniet exercitationem corrupti assumenda, neque voluptates fugit consequatur officia!</p>
 
-        <video className="order-first" src="./test.mp4" controls></video>
+        <video src="./test.mp4" controls></video>
       </article>
     </main>
   );
